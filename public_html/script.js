7fc0b10446ca8cf9a2975f5f4f5a51bc7f07f40c
Charlie Antoun <MadariKanoha@gmail.com>
2015-01-07 11:29:09 -0800

    added two different changes to siblings

--- public_html/script.js
+++ public_html/script.js
@@ -2,7 +2,15 @@
 
 
 
-$("document").ready(function{}{
+$("document").ready(function(){
     $(".youtypeinwhatitis").css("background-color", "yellow");
+ 
+    $('p:last').css({"background-color": "green", "color": "white"});
     
-});
+}); 
+
+//$('#oneButton').bind('click', alertButtonClick);
+//
+//function alertButtonClick() {
+//    alert("ButtonClicked");
+//}
